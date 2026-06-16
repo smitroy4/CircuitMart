3e5a807911018da6df475373b41536bb728bdea7
Smit Roy <smitroy22@gmail.com>
2026-06-16 10:12:10 +0530

    Added Resilience4J into the OrderService, implemented Retry, Rate-Limiter and Circuit Breaker mechanisms

--- order-service/src/main/java/com/smit/circuitmart/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/smit/circuitmart/order_service/service/OrdersService.java
@@ -5,6 +5,9 @@ import com.smit.circuitmart.order_service.entity.OrderItem;
 import com.smit.circuitmart.order_service.entity.OrderStatus;
 import com.smit.circuitmart.order_service.entity.Orders;
 import com.smit.circuitmart.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -33,6 +36,9 @@ public class OrdersService {
         return modelMapper.map(order, OrderRequestDto.class);
     }
 
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
+//    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
         log.info("Calling the createOrder method");
         Double totalPrice = inventoryFeignClient.reduceStocks(orderRequestDto);
@@ -47,6 +53,11 @@ public class OrdersService {
         Orders savedOrder = orderRepository.save(orders);
 
         return modelMapper.map(savedOrder, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable){
+        log.error("Fallback occurred due to : {}", throwable.getMessage());
+        return new OrderRequestDto();
     }
 
 }
