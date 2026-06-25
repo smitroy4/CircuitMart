83683a9e7becf64a67483b4fd70dee422a67bc5f
Smit Roy <smitroy22@gmail.com>
2026-06-25 08:32:47 +0530

    Distributed Tracing with Micrometer, connection with Zipkin still pending

--- api-gateway/src/main/java/com/smit/circuitmart/api_gateway/config/AppConfig.java
+++ api-gateway/src/main/java/com/smit/circuitmart/api_gateway/config/AppConfig.java
@@ -1,20 +1,13 @@
-package com.smit.circuitmart.order_service.config;
+package com.smit.circuitmart.api_gateway.config;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 
     @Bean
     public Capability capability(final MeterRegistry registry) {
